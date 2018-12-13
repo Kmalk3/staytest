2eb0be46eeecf74dab3878b8081f45c2b6b8cfc1
Kmalk3 <44727595+Kmalk3@users.noreply.github.com>
2018-12-13 15:20:17 +0100

    Add files via upload

---
+++ stay.js
@@ -0,0 +1,16 @@
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("521057011274874881").join(); 
+    });
+	
+client.login(process.env.BOT_TOKEN);
